9e4163eb824c80fb6622a21b1952d0ac44cab2c5
Safna Deen <Safna Deen@DESKTOP-K2AQO7D>
2020-10-25 19:19:50 +0530

    Controllers

---
+++ Real-estate business/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Real_estate_business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Real_estate_business.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        private BusinessContext businessContext = new BusinessContext();
+        public ActionResult Index()
+        {
+            List<Branch> AllBranches = businessContext.Branches.ToList();
+            return View(AllBranches);
+        }
+
+        public ActionResult StaffDetail()
+        {
+            List<Staff> AllStaffs = businessContext.Staffs.ToList();
+            return View(AllStaffs);
+        }
+
+        public ActionResult RentDetail()
+        {
+            List<Rent> AllRents = businessContext.Rents.ToList();
+            return View(AllRents);
+        }
+
+
+    }
+}
---
+++ Real-estate business/Controllers/RentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Real_estate_business.Controllers
+{
+    public class RentController : Controller
+    {
+        // GET: Rent
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
